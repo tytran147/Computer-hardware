cdcb8abd275486092d9173862cb136a577e0c249
tytran147 <166063627+tytran147@users.noreply.github.com>
2024-06-13 20:06:49 +0000

    New changes

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const subscribeButton = document.getElementById('subscribeButton');
+    subscribeButton.addEventListener('click', () => {
+        alert('Thank you for subscribing!');
+    });
+});
